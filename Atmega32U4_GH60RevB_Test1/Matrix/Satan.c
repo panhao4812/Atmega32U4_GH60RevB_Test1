908c66a3d26b797798b5fea9c3e3ef909a55efb2
panhao <panhao4812@users.noreply.github.com>
2017-09-25 14:57:36 +0800

    fix satan

--- Atmega32U4_GH60RevB_Test1/Matrix/Satan.c
+++ Atmega32U4_GH60RevB_Test1/Matrix/Satan.c
@@ -4,32 +4,7 @@
 //row D0, D1, D2, D3, D5
 //backlight B6
 //rgb E2
-/* Satan GH60 ANSI layout
-   * ,-----------------------------------------------------------.
-   * | 00 |01| 02| 03| 04| 05| 06| 07| 08| 09| 0a| 0b| 0c| 0d    |
-   * |-----------------------------------------------------------|
-   * | 10  | 11| 12| 13| 14| 15| 16| 17| 18| 19| 1a| 1b| 1c|  1d |
-   * |-----------------------------------------------------------|
-   * | 20    | 21| 22| 23| 24| 25| 26| 27| 28| 29| 2a| 2b| 2d    |
-   * |-----------------------------------------------------------|
-   * | 30     | 32| 33| 34| 35| 36| 37| 38| 39| 3a| 3b|    3d    |
-   * |-----------------------------------------------------------|
-   * | 40 | 41 | 42 |        45             | 4a | 4b | 4c | 4d  |
-   * `-----------------------------------------------------------'
- */
-/* Satan GH60 matrix layout (3c is right of 3d, 3d is shift)
-   * ,-----------------------------------------------------------.
-   * | 00 |01| 02| 03| 04| 05| 06| 07| 08| 09| 0a| 0b| 0c| 0d| 49|
-   * |-----------------------------------------------------------|
-   * | 10  | 11| 12| 13| 14| 15| 16| 17| 18| 19| 1a| 1b| 1c|  1d |
-   * |-----------------------------------------------------------|
-   * | 20    | 21| 22| 23| 24| 25| 26| 27| 28| 29| 2a| 2b| 2c|2d |
-   * |-----------------------------------------------------------|
-   * | 30 | 31| 32| 33| 34| 35| 36| 37| 38| 39| 3a| 3b|  3d | 3c |
-   * |-----------------------------------------------------------|
-   * | 40 | 41 | 42 |        45             | 4a | 4b | 4c | 4d  |
-   * `-----------------------------------------------------------'
- */
+
 uint8_t rowPins[ROWS]={5,6,7,8,23};
 uint8_t colPins[COLS]={21,20,24,10, 9, 4,22, 1, 0,14,13,12,11, 3};
 //                 col F0,F1,E6,C7,C6,B7,D4,B1,B0,B5,B4,D7,D6,B3
@@ -46,15 +21,15 @@ uint8_t hexaKeys0[ROWS][COLS] = {
 	{MACRO2,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9,KEY_0,KEY_MINUS,KEY_EQUAL,KEY_BACKSPACE},
 	{KEY_TAB,KEY_Q,KEY_W,KEY_E,KEY_R,KEY_T,KEY_Y,KEY_U,KEY_I,KEY_O,KEY_P,KEY_LEFT_BRACE,KEY_RIGHT_BRACE,KEY_BACKSLASH},
 	{KEY_CAPS_LOCK,KEY_A,KEY_S,KEY_D,KEY_F,KEY_G,KEY_H,KEY_J,KEY_K,KEY_L,KEY_SEMICOLON,KEY_QUOTE,0x00,KEY_ENTER},
-	{KEY_LEFT_SHIFT,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,KEY_SLASH,0x00,0x00,KEY_RIGHT_SHIFT},
-	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,0x00,KEY_SPACE,0x00,0x00,0x00,KEY_RIGHT_ALT,KEY_FN,KEY_FN, KEY_RIGHT_CTRL}
+	{KEY_LEFT_SHIFT,0x00,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,KEY_SLASH,0x00,KEY_RIGHT_SHIFT},
+	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,KEY_SPACE,0x00,0x00,0x00,0x00,KEY_RIGHT_ALT,KEY_FN,KEY_FN, KEY_RIGHT_CTRL}
 };
 uint8_t hexaKeys1[ROWS][COLS] = {
 	{KEY_TILDE,KEY_F1,KEY_F2,KEY_F3,KEY_F4,KEY_F5,KEY_F6,KEY_F7,KEY_F8,KEY_F9,KEY_F10,KEY_F11,KEY_F12, KEY_DELETE},
 	{KEY_TAB,0x00,KEY_UP,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
 	{KEY_CAPS_LOCK,KEY_LEFT,KEY_DOWN,KEY_RIGHT,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,KEY_ENTER},
-	{KEY_LEFT_SHIFT,KEY_NUM_LOCK,KEY_SCROLL_LOCK,KEY_INSERT,KEY_PRINTSCREEN,MACRO0,MACRO1,MACRO3,AUDIO_VOL_DOWN,AUDIO_VOL_UP,AUDIO_MUTE,0x00,0x00,KEY_RIGHT_SHIFT},
-	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,0x00,KEY_SPACE,0x00,0x00,0x00,KEY_RIGHT_ALT,KEY_FN,KEY_FN,KEY_RIGHT_CTRL}
+	{KEY_LEFT_SHIFT,0x00,KEY_NUM_LOCK,KEY_SCROLL_LOCK,KEY_INSERT,KEY_PRINTSCREEN,MACRO0,MACRO1,MACRO3,AUDIO_VOL_DOWN,AUDIO_VOL_UP,AUDIO_MUTE,0x00,KEY_RIGHT_SHIFT},
+	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,KEY_SPACE,0x00,0x00,0x00,0x00,KEY_RIGHT_ALT,KEY_FN,KEY_FN,KEY_RIGHT_CTRL}
 };
 //keymask_bits:7-press 654-hexatype0 3-press 210-hexatype1
 //type: 1-key 2-modifykey 3-mousekey 4-systemkey 5-consumerkey 6-FN 7-macro
@@ -62,8 +37,8 @@ uint8_t keymask[ROWS][COLS] = {
 	{0x71,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11},
 	{0x11,0x10,0x11,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10},
 	{0x11,0x11,0x11,0x11,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x00,0x11},
-	{0x22,0x11,0x11,0x11,0x11,0x17,0x17,0x17,0x15,0x15,0x15,0x00,0x00,0x22},
-	{0x22,0x66,0x22,0x00,0x00,0x00,0x11,0x00,0x00,0x00,0x22,0x66,0x66,0x22}
+	{0x22,0x00,0x11,0x11,0x11,0x11,0x17,0x17,0x17,0x15,0x15,0x15,0x00,0x22},
+	{0x22,0x66,0x22,0x00,0x00,0x11,0x00,0x00,0x00,0x00,0x22,0x66,0x66,0x22}
 };
 void init_cols(){
 	for ( i=0; i<COLS; i++){
